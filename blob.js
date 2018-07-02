1a82e52ab7b656f883826bb445728793df1b1d85
My Kim Bui <mykimbuibui@gmail.com>
2018-07-02 11:49:24 +0300

    working

--- blob.js
+++ blob.js
@@ -15,25 +15,26 @@ camera.position.z = 5;
 var animate = function () {
   requestAnimationFrame( animate );
 
-  var time = performance.now() * 0.01;
+  var time = performance.now() * 0.001;
   var update = function() {
-    //go through vertices here and reposition them 
-    var k = 3;
-    //for (var i = 0; i < sphere.geometry.vertices.length; i++) {
+    //go through vertices here and reposition them
+    var k = 1;
+    for (var i = 0; i < sphere.geometry.vertices.length; i++) {
+      var p = sphere.geometry.vertices[i];
+      p.normalize().multiplyScalar(1 + 0.3 * noise.perlin3(p.x * k + time, p.y * k, p.z * k));
+    }
 
-//var p = sphere.geometry.vertices[i];
-//p.normalize().multiplyScalar(1 + 0.3 * noise.perlin3(p.x * k + time, p.y * k, p.z * k));
-//}
-
-for (var i = 0; i < sphere.geometry.faces.length; i++) {
-    var uv = sphere.geometry.faceVertexUvs[0][i]; //faceVertexUvs is a huge arrayed stored inside of another array
-    var f = sphere.geometry.faces[i];
-    var p = sphere.geometry.vertices[f.a];//take the first vertex from each face
-    p.add(p.clone().normalize().multiplyScalar(0.1 * noise.perlin3(p.x * k, p.y * k, p.z * k)));
-  }
+// for (var i = 0; i < sphere.geometry.faces.length; i++) {
+//     var uv = sphere.geometry.faceVertexUvs[0][i]; //faceVertexUvs is a huge arrayed stored inside of another array
+//     var f = sphere.geometry.faces[i];
+//     var p = sphere.geometry.vertices[f.a];//take the first vertex from each face
+//     p.add(p.clone().normalize().multiplyScalar(0.1 * noise.perlin3(p.x * k, p.y * k, p.z * k)));
+//   }
   sphere.geometry.computeVertexNormals();
   sphere.geometry.normalsNeedUpdate = true;
+  sphere.geometry.verticesNeedUpdate = true;
 }
+
 update();
 
 sphere.rotation.x += 0.01;
